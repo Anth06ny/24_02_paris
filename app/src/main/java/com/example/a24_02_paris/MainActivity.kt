e299fd6fce108e5f3d7c3e226db40e935b250c18
AnthonyPCPortable <anth06ny@gmail.com>
2024-02-22 17:09:42 +0100

    TP Compose

--- app/src/main/java/com/example/a24_02_paris/MainActivity.kt
+++ app/src/main/java/com/example/a24_02_paris/MainActivity.kt
@@ -1,14 +1,56 @@
 package com.example.a24_02_paris
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.a24_02_paris.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    //version findViewById
+    val et  by lazy { findViewById<EditText>(R.id.et) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        binding.btValidate.setOnClickListener {
+            if (binding.rbLike.isChecked) {
+                et.setText(binding.rbLike.text)
+            }
+            else if (binding.rbDislike.isChecked) {
+                et.setText(binding.rbDislike.text)
+            }
+
+            binding.iv.setImageResource(R.drawable.baseline_flag_24)
+        }
+
+        binding.btCancel.setOnClickListener(this)
+
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onClick(v: View?) {
+        if(v === binding.btCancel){
+            binding.rg.clearCheck()
+            et.setText("")
+
+            startActivity(Intent(this, ComposeExActivity::class.java))
+        }
     }
 
 }
